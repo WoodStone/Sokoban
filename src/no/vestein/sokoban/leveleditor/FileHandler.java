8be4bf30eec40c2f9fb24e0f0ab0d3feed905fc7
WoodStone <golfflames@gmail.com>
2015-02-10 03:44:32 +0100

    Fixed Filehandler

--- src/no/vestein/sokoban/leveleditor/FileHandler.java
+++ src/no/vestein/sokoban/leveleditor/FileHandler.java
@@ -3,7 +3,7 @@ package no.vestein.sokoban.leveleditor;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-//import java.io.FilePermission;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -43,17 +43,27 @@ public class FileHandler {
 		fileChooser.setInitialDirectory(new File(filepath));
 		
 		File file = fileChooser.showOpenDialog(SokobanScene.primaryStage);
+		System.out.println(file.getPath());
 
 		return readLevelFile(file);
 	}
 	
 	public static char[][] loadLevel(String filename) throws FileNotFoundException {
-		File file = new File(Main.class.getResource("map/levels/" + filename).getPath());
-		return readLevelFile(file);
+		InputStream io = Main.class.getResourceAsStream("/no/vestein/sokoban/map/levels/" + filename);
+		return readLevelFile(io);
+	}
+	
+	public static char[][] readLevelFile(InputStream io) throws FileNotFoundException {
+		Scanner scanner = new Scanner(io);
+		return readFile(scanner);
 	}
 	
 	public static char[][] readLevelFile(File file) throws FileNotFoundException {
 		Scanner scanner = new Scanner(file);
+		return readFile(scanner);
+	}
+	
+	private static char[][] readFile(Scanner scanner) {
 		char[][] newLevel = new char[20][20];
 		int yPos = 0;
 		while (scanner.hasNextLine()) {
