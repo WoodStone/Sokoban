67e720a7f000fe6df7f1e7f8d6f53a002e994ed4
WoodStone <golfflames@gmail.com>
2015-02-26 20:46:28 +0100

    Added some effects to the levelselect menu

--- src/no/vestein/sokoban/fxml/LevelSelectStackPane.java
+++ src/no/vestein/sokoban/fxml/LevelSelectStackPane.java
@@ -10,6 +10,7 @@ import javafx.scene.text.Text;
 public class LevelSelectStackPane extends StackPane {
 
 	private int tag;
+	private Rectangle rect;
 	
 	public LevelSelectStackPane(int startx, int starty, int x, int y) {
 		super();
@@ -17,19 +18,36 @@ public class LevelSelectStackPane extends StackPane {
 		setLayoutY(starty + y * 100);
 
 		Rectangle rect = new Rectangle(75, 75);
-		rect.setFill(Color.rgb(128, 128, 128, 0.50));
+		rect.setFill(Color.rgb(128, 128, 128, 0.25));
+		rect.setEffect(new DropShadow(5, Color.GRAY));
 		
 		Text number = new Text(Integer.toString(5 * y + x + 1));
 		number.setFill(Color.AQUAMARINE);
 		number.setFont(Font.font(50));
-		number.setEffect(new DropShadow());
+		number.setEffect(new DropShadow(5, Color.BLACK));
 		
 		this.getChildren().addAll(rect, number);
 		this.tag = 5 * y + x + 1;
+		this.rect = rect;
+		
+		setEvents();
 	}
 	
 	public int getTag() {
 		return tag;
+	}
+	
+	public Rectangle getRectangle() {
+		return rect;
+	}
+	
+	private void setEvents() {
+		this.setOnMouseEntered(mouseEvent -> {
+			rect.setEffect(new DropShadow(5, Color.AQUA));
+		});
+		this.setOnMouseExited(mouseEvent -> {
+			rect.setEffect(new DropShadow(5, Color.BLACK));
+		});
 	}
 	
 }
